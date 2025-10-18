097216c17bd28c960df07bab2cc5e28a50a6e38a
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-18 12:05:41 +0500

    Changes made

--- daystill2030/script.js
+++ daystill2030/script.js
@@ -1,9 +1,7 @@
 var today = new Date();
 var doomsday = new Date("June 30, 2035");
-
 var msToday = today.getTime();
 var msDoomsday = doomsday.getTime();
-
 var msDiff = msDoomsday - msToday;
 var dDiff = Math.floor(msDiff / (1000 * 60 * 60 * 24));
 
