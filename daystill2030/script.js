547189e01ecc13e2b6b650989e7f995d40af18e2
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-18 12:05:28 +0500

    Changes made

---
+++ daystill2030/script.js
@@ -0,0 +1,10 @@
+var today = new Date();
+var doomsday = new Date("June 30, 2035");
+
+var msToday = today.getTime();
+var msDoomsday = doomsday.getTime();
+
+var msDiff = msDoomsday - msToday;
+var dDiff = Math.floor(msDiff / (1000 * 60 * 60 * 24));
+
+console.log("Days till June 30, 2035:", dDiff);
