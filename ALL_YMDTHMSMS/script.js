548c638631321bf33cea1a5969b145f47ac887fe
ArsalJan34 <arsaljaan532@gmail.com>
2025-10-18 12:03:01 +0500

    Changes made

---
+++ ALL_YMDTHMSMS/script.js
@@ -0,0 +1,15 @@
+var d = new Date();
+// getting day giving index hence 0 = sun
+console.log("Day of Week (0=Sun):", d.getDay());
+// getting month giving index because 0 = jan
+console.log("Month (0=Jan):", d.getMonth());
+// getting date
+console.log("Date:", d.getDate());
+// getting full year
+console.log("Year:", d.getFullYear());
+// here we getting all of them i am tired by giving comments to each
+console.log("Hour:", d.getHours());
+console.log("Minutes:", d.getMinutes());
+console.log("Seconds:", d.getSeconds());
+console.log("Milliseconds:", d.getMilliseconds());
+console.log("Milliseconds since 1970:", d.getTime());
